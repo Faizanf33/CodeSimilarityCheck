1a5c46ce5e13acc3c0ef4e98e43e59b507ff540e
Faizan Ahmad <faizanahmad33.fa@gmail.com>
2022-01-04 13:09:34 +0500

    DEBUG = False

--- codeSimilarityCheck/settings.py
+++ codeSimilarityCheck/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-iipt$!3b#sa#da+do2+nd628l@7)w(gqzmgf*gte_66pcdp__m'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
