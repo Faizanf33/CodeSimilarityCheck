9ed906cd87e18f6b1f73219e07d97134c345d247
Faizan Ahmad <faizanahmad33.fa@gmail.com>
2022-01-04 15:48:15 +0500

    path issue resolve 01

--- csc/functions/functions.py
+++ csc/functions/functions.py
@@ -124,11 +124,22 @@ def generate_report(filename, results):
 
     print("Saved report to {}".format(os.path.join(path, 'pdf')))
 
-    # If file exists open it in new tab of current browser
-    if os.path.exists(os.path.join(path, 'pdf', filename.split('.')[0] + '.pdf')):
-        print("File exists")
-        os.system('open -a "Google Chrome" {}'.format(os.path.join(path,
-                  'pdf', filename.split('.')[0] + '.pdf')))
+    # If file exists open it in new tab of current browser for windows
+    if os.name == 'nt':
+        os.startfile(os.path.join(
+            path, 'pdf', filename.split('.')[0] + '.pdf'))
+
+    else:
+        # for mac and linux
+        os.system("open " + os.path.join(path, 'pdf',
+                  filename.split('.')[0] + '.pdf'))
+
+    # # change working directory to current directory
+    # if os.path.exists(os.path.join(path, 'pdf', filename.split('.')[0] + '.pdf')):
+    #     print("File exists")
+
+    #     os.system('open -a "Google Chrome" {}'.format(os.path.join(path,
+    #               'pdf', filename.split('.')[0] + '.pdf')))
 
     # change working directory to current directory
     os.chdir(current_dir)
